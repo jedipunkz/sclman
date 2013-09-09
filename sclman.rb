8de98a06965e8078804174489a321879ff8b5f40
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2013-09-09 17:19:07 +0900

    supported adding / deleting counter

--- sclman.rb
+++ sclman.rb
@@ -23,81 +23,121 @@ $man_flavor = ini.search("MANAGER", "man_flavor")
 $man_image = ini.search("MANAGER", "man_image")
 $man_key = ini.search("MANAGER", "man_key")
 
+def make_shortname(environment)
+  instances = db_search_instance(environment)
+  instance = instances[2].split("web")
+  return instance[0]
+end
+
+def add_server()
+  $wng_group[0].each do |adding|
+    db_count_adding = db_search_count(adding)
+    count_adding = db_count_adding
+    puts "adding server at #{adding}, count_adding : #{count_adding}"
+    shortname = make_shortname(adding)
+    openstack_create_node($man_flavor, $man_image, $man_key, shortname+"web"+count_adding.to_s)
+    ipaddr = openstack_search_ip(shortname)
+    sleep(20)
+    chef_create_node(shortname+"web"+count_adding.to_s, ipaddr, adding, "web")
+    insert_table_lbmembers(shortname+"web"+count_adding.to_s, ipaddr, adding)
+    count_adding += 1
+    update_inc_counter(adding)
+  end
+end
+
+def del_server(group)
+  if group == "stb_group" then
+    $stb_group.each do |deleting|
+      db_count_deleting = db_search_count(deleting)
+        count_deleting = db_count_deleting
+      if count_deleting.to_i >= 4 then
+        puts "deleting server at #{deleting}, count_deleting : #{count_deleting}"
+        shortname = make_shortname(deleting)
+        openstack_delete_node(shortname+"web"+count_deleting.to_s)
+        chef_delete_node(shortname+"web"+count_deleting.to_s)
+        delete_table_lbmembers(shortname+"web"+count_deleting.to_s)
+        update_dec_counter(deleting)
+      end
+    end
+  elsif group === "group_all" then
+    $group_all.each do |deleting|
+      db_count_deleting = db_search_count(deleting)
+      count_deleting = db_count_deleting - 1
+      if count_deleting.to_i >= 4 then
+        puts "deleting server at #{deleting}, count_deleting : #{count_deleting}"
+        shortname = make_shortname(deleting)
+        openstack_delete_node(shortname+"web"+count_deleting.to_s)
+        chef_delete_node(shortname+"web"+count_deleting.to_s)
+        delete_table_lbmembers(shortname+"web"+count_deleting.to_s)
+        update_dec_counter(deleting)
+      end
+    end
+  end
+end
+
 class SclmanDaemon < DaemonSpawn::Base
   def start(args)
     puts "start : #{Time.now}"
+    trig_add = 0; counter_add = 0; trig_del = 0; counter_del = 0
     loop do
       # get all of groups(environmet)
       db_group_all = db_search_group_all()
-      group_all = db_group_all.uniq
-      puts "all of groups : #{group_all}"
+      $group_all = db_group_all.uniq
+      puts "all of groups : #{$group_all}"
 
-      warning_instances = []
-      if group_all != [] then
-        warning_instances = sensu_get_instance_load()
-        groups = []
-        wng_group = []
-        if warning_instances != [] then
-          warning_instances.each do |servers|
-            db_group = db_search_group(servers)
-            groups << db_group
-          end
-          wng_group = groups.uniq
-          puts "warning groups : #{wng_group[0]}"
-          stb_group = group_all - wng_group[0]
-          puts "stability groups : #{stb_group}"
-          # adding server
-          wng_group[0].each do |adding|
-            db_count_adding = db_search_count(adding)
-            # count_adding = db_count_adding - 1
-            count_adding = db_count_adding
-            puts "adding server at #{adding}, count_adding : #{count_adding}"
-            instances = db_search_instance(adding)
-            instance = instances[2].split("web")
-            shortname = instance[0]
-            openstack_create_node($man_flavor, $man_image, $man_key, shortname+"web"+count_adding.to_s)
-            ipaddr = openstack_search_ip(shortname)
-            sleep(20)
-            chef_create_node(shortname+"web"+count_adding.to_s, ipaddr, adding, "web")
-            insert_table_lbmembers(shortname+"web"+count_adding.to_s, ipaddr, adding)
-            count_adding += 1
-            update_inc_counter(adding)
-          end
-        elsif stb_group =! [] then
-          stb_group.each do |deleting|
-            db_count_deleting = db_search_count(deleting)
-            # count_deleting = db_count_deleting - 1
-            count_deleting = db_count_deleting
-            if count_deleting.to_i >= 4 then
-              puts "deleting server at #{deleting}, count_deleting : #{count_deleting}"
-              instances = db_search_instance(deleting)
-              instance = instances[2].split("web")
-              shortname = instance[0]
-              openstack_delete_node(shortname+"web"+count_deleting.to_s)
-              chef_delete_node(shortname+"web"+count_deleting.to_s)
-              delete_table_lbmembers(shortname+"web"+count_deleting.to_s)
-              #count_deleting -= 1
-              update_dec_counter(deleting)
-            end
-          end
-        elsif warning_instances == [] then
-          group_all.each do |deleting|
-            db_count_deleting = db_search_count(deleting)
-            count_deleting = db_count_deleting - 1
-            #count_deleting = db_count_deleting
-            if count_deleting.to_i >= 4 then
-              puts "deleting server at #{deleting}, count_deleting : #{count_deleting}"
-              instances = db_search_instance(deleting)
-              instance = instances[2].split("web")
-              shortname = instance[0]
-              openstack_delete_node(shortname+"web"+count_deleting.to_s)
-              puts shortname+"web"+count_deleting.to_s
-              chef_delete_node(shortname+"web"+count_deleting.to_s)
-              delete_table_lbmembers(shortname+"web"+count_deleting.to_s)
-              #count_deleting -= 1
-              update_dec_counter(deleting)
-            end
-          end
+      warning_instances = sensu_get_instance_load()
+      groups = []
+      $wng_group = []
+      $stb_group = []
+      if warning_instances != [] then
+        warning_instances.each do |servers|
+          db_group = db_search_group(servers)
+          groups << db_group
+        end
+        $wng_group = groups.uniq
+        puts "warning groups : #{$wng_group[0]}"
+        stb_group = $group_all - $wng_group[0]
+        puts "stability groups : #{$stb_group}"
+      end
+
+      if $group_all != [] and warning_instances != [] then
+        if trig_add == 1 then
+          counter_add += 1
+          puts "counter_add : #{counter_add}"
+        else
+          counter_add = 0
+        end
+        if counter_add >= 10 then
+          add_server()
+          counter_add = 0; trig_add = 0; trig_del = 0
+        else
+          trig_add = 1; trig_del = 0
+        end
+      elsif $group_all != [] and $stb_group != [] then
+        if trig_del == 1 then
+          counter_del += 1
+          puts "counter_del : #{counter_del}"
+        else
+          counter_del = 0
+        end
+        if counter_del >= 10 then
+          del_server("stb_group")
+          counter_del = 0; trig_add = 0; trig_del = 0
+        else
+          trig_add = 0; trig_del = 1
+        end
+      elsif $group_all != [] and warning_instances == [] then
+        if trig_del == 1 then
+          counter_del += 1
+          puts "counter_del : #{counter_del}"
+        else
+          counter_del = 0
+        end
+        if counter_del >= 10 then
+          del_server("group_all")
+          counter_del = 0; trig_add = 0; trig_del = 0
+        else
+          trig_add = 0; trig_del = 1
         end
       end
       sleep(5)
