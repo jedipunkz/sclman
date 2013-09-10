b74f97548feceb070198ec5e03caa371c6ba9b7f
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2013-09-10 12:37:07 +0900

    removed .chef

--- .chef/knife.rb
+++
@@ -1,8 +0,0 @@
-log_level                :info
-log_location             STDOUT
-node_name                'thirai'
-client_key               '/home/thirai/sclman/.chef/thirai.pem'
-validation_client_name   'chef-validator'
-validation_key           '/home/thirai/sclman/.chef/chef-validator.pem'
-chef_server_url          'https://10.200.10.96'
-syntax_check_cache_path  '/home/thirai/sclman/.chef/syntax_check_cache'
